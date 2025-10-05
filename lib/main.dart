7cf282be16d21ca5085cacedfaba176a8fc4f977
Pedro Araoz <pedroaraozc@gmail.com>
2025-10-04 21:55:20 -0300

    Falta que la edicion del perfil impacte en el back

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:go_app_flutter/screens/perfil_screen.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+       return MaterialApp(
+      title: 'Mi App',
+      debugShowCheckedModeBanner: false,
+
+      // 👇👇 Agregá esto 👇👇
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('es', 'ES'), // Español
+        Locale('en', 'US'), // Inglés (por si acaso)
+      ],
+
+      home: const PerfilScreen(),
+    );
+  }
+}
