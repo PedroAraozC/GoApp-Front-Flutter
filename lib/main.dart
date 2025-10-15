d4bd0606b56262e53a6879810a0ad408f031ccad
Omar Adra <adra.omar2@gmail.com>
2025-10-15 02:38:26 -0300

    Creada vista de cambiar password (sin backend)

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:go_app_flutter/screens/passwordRecovery/password_recovey.dart';
 import 'package:go_app_flutter/screens/perfil_screen.dart';
 import 'package:go_app_flutter/screens/passwordRecoveryCode/password_recovery_code.dart';
+import 'package:go_app_flutter/screens/passwordChange/password_change.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
 
@@ -29,7 +30,7 @@ class MainApp extends StatelessWidget {
         Locale('en', 'US'), // Inglés (por si acaso)
       ],
 
-      home: const RecuperarPasswordCodeScreen(),
+      home: const ChangePasswordScreen(),
     );
   }
 }
