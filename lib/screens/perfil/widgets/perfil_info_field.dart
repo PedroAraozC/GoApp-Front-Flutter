68a9592beb39812c4b88d043e7e74fd5d851e1f6
Pedro Aráoz <pedroaraozc@gmail.com>
2025-11-06 12:56:35 -0300

    se modifico la pantalla de metodos de pago

--- lib/screens/perfil/widgets/perfil_info_field.dart
+++ lib/screens/perfil/widgets/perfil_info_field.dart
@@ -213,7 +213,7 @@ class PerfilInfoField extends StatelessWidget {
       height: kInputHeight,
       child: DropdownButtonFormField<String>(
         isExpanded: true,
-        value: safeValue,
+        initialValue: safeValue,
         onChanged: (value) {
           if (value != null && onGeneroChanged != null) {
             onGeneroChanged!(value);
